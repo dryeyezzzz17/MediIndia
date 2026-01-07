ab7f87facab9d8f843a1e15634a60ba789ba42f8
Govind Trivedi <govindtrivedi158@gmail.com>
2026-01-07 14:36:15 +0530

    Initial file and folder stepup is done server is running and mongodb atlas connected successfully

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const cors=require("cors");
+require("dotenv").config();
+const connectDb=require("./config/db");
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("MediIndia API is running");
+})
+
+connectDb();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>MediGo India</h1>
+      <p>Medical Tourism Platform</p>
+    </div>
+  )
+}
+
+export default App
