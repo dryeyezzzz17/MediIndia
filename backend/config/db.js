52a3f392837a48761d432e946f467a6865e056d7
Govind Trivedi <govindtrivedi158@gmail.com>
2026-01-15 16:04:13 +0530

    configured the db so that autoIndexing works

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI,{
+            autoIndex:true
+        });
         console.log("✅ MongoDB connected successfully");
     } catch (error) {
         console.error("❌ MongoDB connection failed:", error.message);
